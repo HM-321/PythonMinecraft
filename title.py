71557f90902a24af65f216ad0e716617852cf15d
nicemorningg <goodmorningg@proton.me>
2026-07-12 23:28:21 +0900

    タイトルをMINECRAFTに

--- title.py
+++ title.py
@@ -23,7 +23,7 @@ class TitleScreen:
                z=1)
 
         # タイトル
-        Text(parent=self.root, text='ミネ CRAFT',
+        Text(parent=self.root, text='MINECRAFT',
              origin=(0, 0), y=0.25, scale=5, color=color.white)
 
         Text(parent=self.root, text='Python + Ursina',
