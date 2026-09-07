e799be1ceef57434d7cc3126b04b64c7f389138e
HM-321 <sanjingkuanren@gmail.com>
2026-09-07 22:27:57 +0900

    Set default FPS limit to 60

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ DEFAULT_SETTINGS = {
     'render_distance': 20,
     'bgm_volume': 0.3,
     'se_volume': 0.5,
-    'max_fps': 30,
+    'max_fps': 60,
     'last_server_host': '192.168.0.1',
     'last_server_port': 25565,
     'key_jump': 'space',
